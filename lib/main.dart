eb25db4146d591815ac2da2f98c722b02177922d
David Kalil Braga <dmmd10@hotmail.com>
2021-06-16 12:09:09 -0300

    relembrando layout builder e breakpoints

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_web_aula/orientacao.dart';
+import 'package:flutter_web_aula/regras_layout.dart';
 import 'package:flutter_web_aula/responsividade_wrap.dart';
 import 'package:flutter_web_aula/responsividade_media_query.dart';
 import 'package:flutter_web_aula/responsividade_row_col.dart';
@@ -9,7 +10,8 @@ void main() {
    // home: ResponsividadeMediaQuery(),
    // home: ResponsividadeRowCol(),
     //home: ResponsividadeWrap(),
-    home: Orientacao(),
+   // home: Orientacao(),
+    home: RegrasLayout(),
     debugShowCheckedModeBanner: false,
   ));
 }
